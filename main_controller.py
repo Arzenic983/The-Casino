58dd53e40ab7a142d2f0f792469f1cc5b4b24bbe
Arzenic983 <ArsikXX@gmail.com>
2021-04-23 15:32:54 +1000

    промежуточный коммит

--- main_controller.py
+++ main_controller.py
@@ -1,24 +1,30 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['POST', 'GET'])
 def index():
-    return """
-         <!doctype html>
-                <html lang="en">
-                    <head>
-                        <meta charset="utf-8">                    
-                        <title>Azino777</title>
-                    </head>
-                    <body>
-                        <h1>Арабское казино<h1>
-                    </body>
-                    </html>
-        """
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method == 'POST':
+        return redirect('/registration')
+
+
+@app.route('/registration')
+def registration():
+    return render_template("registration.html")
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+    # form = LoginForm()
+    # if form.validate_on_submit():
+        # return redirect('/success')
+    # return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
+
